21a47af32857eb40ec8cd39be7517dbcb4a95ec2
Roksana Dzioba <roksanadzioba@gmail.com>
2020-01-26 16:48:25 +0100

    Updates

---
+++ precache-manifest.c1c13634edd63aaf31f5b4729c9a7535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c97ea5ecf228cd9a0c8d737f289f1e",
+    "url": "/react-app-crypto/index.html"
+  },
+  {
+    "revision": "2e80d2f01607e329f7c1",
+    "url": "/react-app-crypto/static/css/main.8e842203.chunk.css"
+  },
+  {
+    "revision": "be8661f36bdba305fc8b",
+    "url": "/react-app-crypto/static/js/2.0467b526.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app-crypto/static/js/2.0467b526.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e80d2f01607e329f7c1",
+    "url": "/react-app-crypto/static/js/main.38d7686e.chunk.js"
+  },
+  {
+    "revision": "20c1be3925c4353becd3",
+    "url": "/react-app-crypto/static/js/runtime-main.33dfb8ad.js"
+  }
+]);
